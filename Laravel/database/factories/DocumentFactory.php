51bcbd585da28dd80de2923dcbcba1fdec3a5b35
Jovan Vasic <jovanjovanvasicvasic@gmail.com>
2024-02-04 13:59:44 +0100

    Dodat DocumentFactory

---
+++ Laravel/database/factories/DocumentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Document>
+ */
+class DocumentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'filename' => $this->faker->word,
+            'user_id' => function () {
+                return \App\Models\User::inRandomOrder()->first()->id;
+            },
+        ];
+    }
+}
